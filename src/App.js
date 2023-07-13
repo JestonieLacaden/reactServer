3d65278572b6ed6a6b1323b3ffe3e3fd7331adcd
JestonieLacaden <lacadenjestonie28@gmail.com>
2023-07-13 15:44:04 +0800

    Adding some connections in Server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [listInput, setListInput] = useState("")
+  const [list, setList] = useState([])
+
+  const ApiLink = async () => {
+    const res = await axios.get('http://localhost:3001/lists')
+    setList(res.data)
+  };
+
+  useEffect(() => {
+    ApiLink()
+  }, [])
+
+
+
+  const sendList = async () => {
+    await axios.post("http://localhost:3001/lists", { listInput })
+    setList([...list, { listInput }])
+  }
   return (
+
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container d-flex justify-content-center flex-column align-items-center'>
+        <h1>Todo List</h1>
+        <input type='text' value={listInput} onChange={(event) => setListInput(event.target.value)} placeholder='Type your Todo List'></input>
+        <button className='btn btn-primary mt-2'>Add to do List</button>
+        <h3 className='align-self-start ms-4'>LIST</h3>
+        <ul className='align-self-start'>
+          {list.map((listInput) => (
+            <li>{listInput.listInput}</li>
+          ))}
+          
+        </ul>
+      </div>
     </div>
   );
 }
